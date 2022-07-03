ebe9072ca237c85361130f407488115f9ea2bb1f
ecc3 <liz3.33zie@yahoo.co.uk>
2022-07-03 18:56:30 +0100

    added server

---
+++ server/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { json } from "body-parser";
+import {
+  addProduct,
+  deleteProductById,
+  getProductById,
+  updateProductById
+} from "./controllers/productsController";
+
+const port = 8080;
+const host = "0.0.0.0";
+const app = express();
+
+app.use(json());
+app.use(express.json());
+
+app.get("/api/product/:productId", getProductById);
+app.post("/api/product", addProduct);
+app.put("/api/product/:productId", updateProductById);
+app.delete("/api/product/:productId", deleteProductById);
+
+app.listen(port, host);
+console.log(`Running on http://${host}:${port}`);
